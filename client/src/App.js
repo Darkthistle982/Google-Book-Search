c3b8916a2337882598759fc0febd3290a653992f
Brian Allen <brianallen2401@gmail.com>
2020-05-13 14:32:07 -0600

    transferred searchbar logic to app.js. This way search data can be saved to state easier.

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,49 @@
 import React, { Component } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Jumbotron from "./Components/Jumbotron/Jumbotron";
-import SearchBar from "./Components/SearchBar/SearchBar";
+import API from "../src/utils/API";
 import ResultCardContainer from "./Components/ResultCardContainer/ResultCardContainer";
 import "./App.css";
 
 class App extends Component {
   state = {
+    searchTerm: "",
     booklist: [
       {
         title: "stuff",
         authors: ["people", "otherpeople"],
         description: "stuf stuf stuf",
         thumbnail: "https://i.imgur.com/N8bGxz7.png",
-        link: "www.linkypoo.corn"
+        link: "www.linkypoo.corn",
       },
       {
         title: "other stuff",
         authors: ["people", "otherpeople"],
         description: "stuf stuf stuf",
-        thumbnail: "https://i.imgur.com/LVJhJrk.jpg"
+        thumbnail: "https://i.imgur.com/LVJhJrk.jpg",
       },
       {
         title: "more stuff",
         authors: ["people", "otherpeople"],
         description: "stuf stuf stuf",
-        thumbnail: "https://i.imgur.com/rpgzGgh.jpg"
+        thumbnail: "https://i.imgur.com/rpgzGgh.jpg",
       },
     ],
   };
 
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    API.search(this.state.searchTerm).then(function (data) {
+      console.log(data.data.items[0]);
+    });
+  };
 
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      searchTerm: value,
+    });
+  };
 
   render() {
     return (
@@ -38,8 +51,32 @@ class App extends Component {
         <div>
           <Navbar />
           <Jumbotron />
-          <SearchBar />
-          <ResultCardContainer booklist={this.state.booklist}/>
+          <div className="container">
+            <form
+              className="mt-2 p-2 border border-dark bg-light"
+              onSubmit={this.handleFormSubmit}
+            >
+              <div className="form-group">
+                <h1>Find a Book to Explore!</h1>
+                <hr />
+                <label htmlFor="searchBar" className="mr-1">
+                  Search for Books
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="searchBar"
+                  value={this.state.searchTerm}
+                  onChange={this.handleInputChange}
+                  name="searchTerm"
+                ></input>
+              </div>
+              <button type="submit" className="btn btn-dark">
+                Search
+              </button>
+            </form>
+          </div>
+          <ResultCardContainer booklist={this.state.booklist} />
         </div>
       </div>
     );
